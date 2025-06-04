89a22300183269ea907d2e7ff78366090bba2819
Guillaume <guillaume.thouvenin@vates.tech>
2025-06-04 21:21:07 +0200

    [rlox-server] implement the parsing of http request

--- rlox-server/src/http_message.rs
+++ rlox-server/src/http_message.rs
@@ -4,20 +4,25 @@
 //                    CRLF
 //                    [ message-body ]
 
-use std::error::Error;
-use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Read};
 use std::net::TcpStream;
 
 const CRLF: &str = "\r\n";
 
 pub struct HttpMessage {
     start_line: String,
-    field_line: Vec<String>, // can be empty
-    msg_body: Vec<u8>,       // Body can be gzip for example
+    fields: HashMap<String, String>, // can be empty
+    body: Vec<u8>,                   // Body can be gzip for example
 }
 
 pub enum HttpMessageError {
     StartLineNotFound,
+    StartLineWrongEnd,
+    FieldLineWrongEnd,
+    FieldLineParseError,
+    BodyLenReadError,
+    BodyLenParseError,
 }
 
 impl HttpMessage {
@@ -26,20 +31,64 @@ impl HttpMessage {
         // The normal procedure for parsing an HTTP message is to read the
         // start-line into a structure.
         let mut start_line = String::new();
-        let sz = match req.read_line(&mut start_line) {
-            Err(e) => {
-                eprintln!("Failed to read start_line: {}", e);
-                return Err(HttpMessageError::StartLineNotFound);
-            }
-            Ok(sz) => sz,
-        };
+        let sz = req.read_line(&mut start_line).map_err(|e| {
+            eprintln!("Failed to read start_line: {}", e);
+            HttpMessageError::StartLineNotFound
+        })?;
 
         println!("Read {} bytes: <{:?}>", sz, start_line);
 
+        // Sanity check: ensure that we read start-line
+        if !start_line.ends_with(CRLF) {
+            return Err(HttpMessageError::StartLineWrongEnd);
+        }
+
+        let mut fields: HashMap<String, String> = HashMap::new();
+        loop {
+            let mut field_line = String::new();
+            let _sz = req.read_line(&mut field_line).unwrap_or(0);
+
+            if field_line.is_empty() || field_line == CRLF {
+                // We reached the end of fields
+                break;
+            }
+
+            if !field_line.ends_with(CRLF) {
+                return Err(HttpMessageError::FieldLineWrongEnd);
+            }
+
+            let (f_name, f_value) = field_line
+                .split_once(':')
+                .ok_or(HttpMessageError::FieldLineParseError)
+                .map(|(s1, s2)| (s1.trim().to_lowercase().to_string(), s2.trim().to_string()))?;
+
+            // TODO: A server MUST reject, with a response status code of 400 (Bad Request),
+            // any received request message that contains whitespace between a header field
+            // name and colon. So don't trim as we did and check... but currently we don't
+            // care.
+            println!("Field added: <{}>/<{}>", f_name, f_value);
+            fields.insert(f_name, f_value);
+        }
+
+        // There is a body if content-lenght is present. Otherwise just return
+        // an empty vec.
+        let body: Vec<u8> = if let Some(value) = fields.get("content-length") {
+            let body_len = value
+                .parse::<usize>()
+                .map_err(|_| HttpMessageError::BodyLenParseError)?;
+
+            let mut body = vec![0u8; body_len];
+            req.read_exact(&mut body)
+                .map_err(|_| HttpMessageError::BodyLenReadError)?;
+            body
+        } else {
+            Vec::new()
+        };
+
         Ok(HttpMessage {
             start_line,
-            field_line: Vec::new(),
-            msg_body: Vec::new(),
+            fields,
+            body,
         })
     }
 }
