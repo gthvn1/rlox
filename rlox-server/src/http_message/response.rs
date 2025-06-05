1c9e5afaf04ab78f212143fe8b765942c756d090
Guillaume <guillaume.thouvenin@vates.tech>
2025-06-05 21:32:38 +0200

    [rlox-server] use constant for path, file and type

--- rlox-server/src/http_message/response.rs
+++ rlox-server/src/http_message/response.rs
@@ -2,13 +2,32 @@ use std::fs;
 
 use crate::http_message::request::Request;
 
+pub const PATH_INDEX_HTML: &str = "/index.html";
+pub const PATH_ROOT: &str = "/";
+pub const PATH_STYLE_CSS: &str = "/css/style.css";
+pub const PATH_JS: &str = "/rlox_web.js";
+pub const PATH_WASM: &str = "/rlox_web_bg.wasm";
+pub const PATH_FAVICON: &str = "/favicon.ico";
+
+pub const FILE_INDEX_HTML: &str = "./static/index.html";
+pub const FILE_STYLE_CSS: &str = "./static/css/style.css";
+pub const FILE_JS: &str = "./static/rlox_web.js";
+pub const FILE_WASM: &str = "./static/rlox_web_bg.wasm";
+pub const FILE_FAVICON: &str = "./static/favicon/favicon-32x32.png";
+
+pub const TYPE_HTML: &str = "text/html";
+pub const TYPE_CSS: &str = "text/css";
+pub const TYPE_JS: &str = "text/javascript";
+pub const TYPE_WASM: &str = "application/wasm";
+pub const TYPE_PNG: &str = "image/png";
+
 pub fn to_response(req: Request) -> Vec<u8> {
     match req.get_path() {
-        Some("/css/style.css") => serve_file("./static/css/style.css", "text/css"),
-        Some("/rlox_web.js") => serve_file("./static/rlox_web.js", "text/javascript"),
-        Some("/rlox_web_bg.wasm") => serve_file("./static/rlox_web_bg.wasm", "application/wasm"),
-        Some("/favicon.ico") => serve_file("./static/favicon/favicon-32x32.png", "image/png"),
-        Some("/") | Some("/index.html") => serve_file("./static/index.html", "text/html"),
+        Some(PATH_ROOT) | Some(PATH_INDEX_HTML) => serve_file(FILE_INDEX_HTML, TYPE_HTML),
+        Some(PATH_STYLE_CSS) => serve_file(FILE_STYLE_CSS, TYPE_CSS),
+        Some(PATH_JS) => serve_file(FILE_JS, TYPE_JS),
+        Some(PATH_WASM) => serve_file(FILE_WASM, TYPE_WASM),
+        Some(PATH_FAVICON) => serve_file(FILE_FAVICON, TYPE_PNG),
         Some(path) => {
             eprintln!("Cannot serve path ({})", path);
             todo!()
